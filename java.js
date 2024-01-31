dca28947876151f2557fdbbb16053e6e9e09f3a0
muhamad-rizky <rizkyfauzi062006@gmail.com>
2024-01-31 12:46:59 +0700

    first upload

---
+++ java.js
@@ -0,0 +1,117 @@
+// LKPD 2
+// NO 1  menghitung gaji pokok //
+
+// let nama = 'rizky';
+// let gaji_pokok = 100000;
+
+// let tunjangan = 0.2 * gaji_pokok;
+// console.log(tunjangan)
+// let pajak = 0.15 * gaji_pokok + tunjangan;
+// console.log(pajak)
+
+// let gajibersih = gaji_pokok + tunjangan - pajak;
+// console.log (nama + gajibersih);
+
+// NO 2
+
+// let jam = 3600;
+// let menit = 1800;
+// let detik = 40;
+
+// let semuatotal= jam + menit + detik;
+// console.log(semuatotal);
+
+// NO 3
+
+// let totaldetik=5440;
+
+// let jam = Math.floor(totaldetik / 3600) ;
+// let sisadetik =Math.floor(totaldetik % 3600);
+// console.log(jam + 'jam')
+
+// let menit =Math.floor(sisadetik / 60) ;
+// console.log(menit + 'menit')
+
+// let detik = sisadetik % 60 ;
+// console.log  (detik +'detik')
+
+
+// LKPD LOOPING
+
+
+// looping for
+// looping 
+// 1.inisialisasi atau kondisi awwal
+// 2.kondisi akhir/penghentian
+// 3. iterasi inc atau dec
+// 4.proses
+
+// for (let a=1; a<=50; a++){
+//     if ( a % 2 == 0){
+//         maka = "genap"
+//     }else {
+//         maka= "ganjil"
+//     }
+// console.log (a + maka);
+// }
+
+// while
+// let b = 1; 
+// while (b <= 50){
+//     if (b % 2 == 0){
+//         console.log (`${b} bilangan genap`)
+//     }else {
+//         console.log (`${b} bilangan ganjil`)
+//     }
+//     b++;
+// }
+
+// do while
+// let c = 1;
+// do {
+//     if (c % 2 == 0) {
+//         console.log (`${c} genap`)
+//     }
+//     else {
+//         console.log (`${c} ganjil`)
+//     }
+// c++;
+
+// }
+// while (c <=10);
+
+// SOAL N0 3
+
+// let n = prompt ("masukan number");
+// let hasil =1;
+
+// for (let m = 2; m <= n; m++) {
+//     hasil *= m;
+
+// }
+
+// console.log (`jika m adalah ${n} maka u adalah ${hasil}  `)
+
+// SOAL NO 4
+
+// let a = 5;
+// let faktorial =1;
+// let b = 2;
+
+// while (b <= a){
+//     faktorial *= b;
+//     b++
+// }
+// console.log (`faktorial dari ${a} adalah ${faktorial}`)
+
+// SOAL NO 5
+
+// let number = prompt ("sadboy")
+// let factorial = 1
+// let b = 1
+
+// do {
+//     factorial *= b
+//     b++;
+// }while( b<=number);
+// console.log (`${number} factorial ${factorial}`)
